f6a13e8a877ab2d816b0f652dad860bfc613281a
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-25 20:53:59 +0800

    Update Buy.jsx

--- src/Components/Buy.jsx
+++ src/Components/Buy.jsx
@@ -5,11 +5,7 @@ const Buy = () => {
     <section className="py-16 w-full bg-[#FFF6E7]">
       <div className="container flex justify-center items-center">
         <iframe
-          src="https://voltichange.net/api/widget/?chain=1&
-darktheme=false&
-tokenin=Native&
-tokenout=0x7f792db54B0e580Cdc755178443f0430Cf799aCa&
-slippage=1"
+          src="https://poocoin.app/tokens/0x06207a94ea8074b12e8e9119d638731d1ac80d48"
           frameborder="0"
           className="voltichange-widget"
         ></iframe>
