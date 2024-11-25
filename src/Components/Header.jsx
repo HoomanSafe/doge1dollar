632f1b2741669448899cc5b9d5c30e7c62262aa1
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-25 20:57:24 +0800

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
       <div className="container flex items-center justify-center">
         <nav className="flex w-full py-3 px-4  bg-white font-Modak rounded-[100px] justify-between items-center">
           <a href="" className="text-lime-600 text-[32px] font-normal">
-            $SCHEME
+            $DOGE1
           </a>
           <div className="justify-start items-start gap-6 hidden  lg:inline-flex">
             <div className="px-5 py-[3px] bg-white rounded-[100px] justify-center items-center gap-2.5 flex">
               <div className="text-lime-600 text-2xl font-normal leading-loose">
-                Tokomomic
+                Tokenomic
               </div>
             </div>
             <div className="px-5 py-[3px] bg-white rounded-[100px] justify-center items-center gap-2.5 flex">
