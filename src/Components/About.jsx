f12d1631580eb47020d84fd262f46e0925ddae65
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-25 20:51:47 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/doge1logo-removebg-preview.png";
 
 const About = () => {
   return (
@@ -7,12 +7,11 @@ const About = () => {
       <div className="container grid grid-cols-1 lg:grid-cols-2 justify-between items-center gap-16">
         <article className="flex-col justify-start items-start gap-3 inline-flex">
           <div className="text-lime-600 text-7xl font-Modak leading-[80px]">
-            $CHEME
+            DOGE1$
           </div>
           <div className=" text-stone-950 text-xl font-normal">
-            Introducing "EthEmpire: The 90% Revolution" – where we're flipping
-            the crypto world on its head and making it rain with laughter and
-            riches! 🎉💰
+           The $1 target represents more than just a number—it symbolizes the collective strength of the Dogecoin community
+            . By banding together, we can bring awareness, excitement, and investment that could make $1 a reality.
           </div>
         </article>
 
